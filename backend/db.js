eebb34648844573cb8a0979c9bcc66ea267bdac7
anujrajincludemyself <anujraj24go@gmail.com>
2026-01-10 17:48:23 +0530

    db updates

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
-const dotenv = require ("dotenv")
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const mongodb = async () => {
   try {
     await mongoose.connect(process.env.mongoURI);
     console.log("MongoDB Connected Successfully");
+
+    // Fetch food_items collection
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+
+    const data = await fetched_data.find({}).toArray();
+    console.log("Food Items:", data);
+
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
   }
---
+++ backend/models/user.js
@@ -0,0 +1,5 @@
+// const mongoose = require("mongoose")
+
+// const newschema = mongoose.Schema(){
+
+// }
