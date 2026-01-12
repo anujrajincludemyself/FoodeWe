fa0008ddbba85b4bc4784d0388633198cc05b0dd
anujrajincludemyself <anujraj24go@gmail.com>
2026-01-12 08:46:08 +0530

    corrected apis

--- frontend/src/components/Carousal.js
+++ frontend/src/components/Carousal.js
@@ -7,20 +7,20 @@ export default function Carousel() {
             <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
 
                 <div className="carousel-inner " id='carousel'>
-                    <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+                    <div className=" carousel-caption  " style={{ zIndex: "9" }}>
                         <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
                             <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
                             <button className="btn text-white bg-success" type="submit">Search</button>
                         </form>
                     </div>
                     <div className="carousel-item active" >
-                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://picsum.photos/900/700?random=1" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="Burger" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://picsum.photos/900/700?random=2" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="Pastry" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                        <img src="https://picsum.photos/900/700?random=3" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="Barbeque" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
